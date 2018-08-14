8aedc529e11f89aa22652efdeaf178b9a6f231a6
youssef333 <40820697+youssef333@users.noreply.github.com>
2018-08-14 19:25:47 +0200

    Add files via upload

--- src/book.js
+++ src/book.js
@@ -22,30 +22,29 @@ class book extends Component {
 
 		return (
 
-<li>
-  <div className="book">
-    <div className="book-top">
-      <div className="book-cover" style={style}>
-      </div>
-      <div className="book-shelf-changer">
-        <select 
-                value={this.props.book.shelf || 'none'} 
-                onChange={(event) => this.handleChange(event.target.value)}>
-                      <option value="move" disabled>Move to...</option>
-                      <option value="currentlyReading">Currently Reading</option>
-                      <option value="wantToRead">Want to Read</option>
-                      <option value="read">Read</option>
-                      <option value="none">None</option>
-        </select>
-      </div>
-    </div>
-    <div className="book-title">{this.props.book.title}</div>
-    <div className="book-authors">
-    {this.props.book.authors ? this.props.authors.join(', ') : ''}
-    </div>
+	<li>
+	  <div className="book">
+	    <div className="book-top">
+	      <div className="book-cover" style={style}></div>
+	      	<div className="book-shelf-changer">
+	        	<select 
+	                value={this.props.book.shelf || 'none'} 
+	                onChange={(event) => this.handleChange(event.target.value)}>
+	                      <option value="move" disabled>Move to...</option>
+	                      <option value="currentlyReading">Currently Reading</option>
+	                      <option value="wantToRead">Want to Read</option>
+	                      <option value="read">Read</option>
+	                      <option value="none">None</option>
+	        	</select>
+	      </div>
+	    </div>
+	    	<div className="book-title">{this.props.book.title}</div>
+	    		<div className="book-authors">
+	    			{this.props.book.authors ? this.props.authors.join(', ') : ''}
+	    		</div>
 
-  </div>
-</li>
+	  </div>
+	</li>
     )
   }
 }
